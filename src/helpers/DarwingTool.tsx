06a1c0f3b7fdb0a6008e0863684fbda9fd196f92
msamy2019 <msamy2019@gmail.com>
2023-06-18 04:25:48 +0300

    refactor event listeners in drawingtool class

--- src/helpers/DarwingTool.tsx
+++ src/helpers/DarwingTool.tsx
@@ -6,21 +6,21 @@ import {
 } from "cesium";
 import { bearing, point, polygon, transformRotate } from '@turf/turf'
 export class DrawingTool {
-    viewer: Viewer
-    shapePoints: Array<Cartesian3>
+    private viewer: Viewer
+    private shapePoints: Array<Cartesian3>
     isDrawingActive: boolean
     currentDrawingShape: Entity | null;
     shapePointsLength: number
-    eventHandler: ScreenSpaceEventHandler
+    private eventHandler: ScreenSpaceEventHandler
     mode: 'select' | 'draw'
     selectionBoundry: Entity | null
-    selectionBoundryPoints: Array<Entity>
+    private selectionBoundryPoints: Array<Entity>
     isDraging: boolean
-    startDragingPostion: Cartesian3 | null
+    startDragingPostion: Cartesian3 |Cartesian2 |null
     selectedCenter: Array<number>
     selectedEntitiy: Entity | null
-    selectedLatLngInitPostions: Array<Array<number>>
-    selectedPostions: { positions: Array<Cartesian3> }
+    private selectedLatLngInitPostions: Array<Array<number>>
+    private selectedPostions: { positions: Array<Cartesian3> }
     constructor(_viewer: Viewer) {
         this.viewer = _viewer
         this.shapePoints = []
@@ -53,113 +53,114 @@ export class DrawingTool {
         this.selectionBoundry = null
 
     }
+    private onLeftClick(event: ScreenSpaceEventHandler.PositionedEvent) {
+        if (this.mode === 'draw') {
+            this.isDrawingActive = true
+            var newPosition = this.viewer.camera.pickEllipsoid(event.position);
+            if (!this.currentDrawingShape) {
+                var dynamicPositions = new CallbackProperty((() => {
+                    return { positions: this.shapePoints };
+                }), false);
+                this.currentDrawingShape = this.viewer.entities.add({
+                    polygon: {
+                        hierarchy: dynamicPositions,
+                        height: 0,
+                        material: Color.RED.withAlpha(0.5),
+                        outline: true,
+                        outlineWidth: 4,
+                        outlineColor: Color.BLACK,
+                    },
+                });
+            }
+            this.shapePoints.push(newPosition as Cartesian3)
+            this.shapePointsLength++
+        } else if (this.mode === "select") {
+            let picked = this.viewer.scene.pick(event.position)
+            this.onSelectObject(picked?.id)
+        }
+    }
+    private onLeftDown(event: ScreenSpaceEventHandler.PositionedEvent) {
+        let entity: Entity = this.viewer.scene.pick(event.position)
+        if (entity) {
+            this.isDraging = true
+            this.startDragingPostion = event.position
+            this.viewer.scene.screenSpaceCameraController.enableRotate = false
+        }
+    }
+    private onLeftUp(event: ScreenSpaceEventHandler.PositionedEvent)  {
+        this.isDraging = false
+        this.viewer.scene.screenSpaceCameraController.enableRotate = true
+    }
+    private onMouseMove(event: ScreenSpaceEventHandler.MotionEvent)  {
+        if (this.isDrawingActive) {
+            var newPosition = this.viewer.camera.pickEllipsoid(event.endPosition);
+            if (this.shapePointsLength < this.shapePoints.length)
+                this.shapePoints.pop()
+
+            this.shapePoints.push(newPosition as Cartesian3)
+        }
+        if (this.isDraging && this.startDragingPostion && this.selectedEntitiy && this.selectedLatLngInitPostions.length) {
+            //@ts-ignore
+            let rotationAngle = this.getRotationAngel(this.selectedCenter,
+                this.viewer.camera.pickEllipsoid(this.startDragingPostion),
+                this.viewer.camera.pickEllipsoid(event.endPosition)
+            )
+            let rotatedPolygon = transformRotate(polygon([[...this.selectedLatLngInitPostions, this.selectedLatLngInitPostions[0]]]), rotationAngle, { pivot: this.selectedCenter })
+            let Cart3Coordinate = rotatedPolygon.geometry.coordinates[0].map(ele => {
+                return Cartesian3.fromDegrees(ele[0], ele[1])
+            })
+            Cart3Coordinate.pop()
+
+            if (this.selectedEntitiy)
+                this.selectedPostions.positions = Cart3Coordinate
+
+        }
+        // 
+
+    }
+    private onDoubleClick(event: ScreenSpaceEventHandler.PositionedEvent){
+        if (this.isDrawingActive) {
+            this.isDrawingActive = false
+            let points = { positions: [...this.shapePoints] }
+            let entity = this.viewer.entities.add({
+                polygon: {
+                    hierarchy: new CallbackProperty(() => {
+                        return { positions: points.positions }
+                    }, false)
+                    ,
+                    height: 0,
+                    material: Color.WHITE.withAlpha(0),
+                    outlineWidth: 9,
+                    outline: true,
+                    outlineColor: Color.BLACK,
+                },
+
+            });
+            //@ts-ignore
+            entity.points = points
+            if (this.currentDrawingShape) {
+                this.viewer.entities.remove(this.currentDrawingShape)
+            }
+
+            this.currentDrawingShape = null
+            this.shapePoints = []
+            this.shapePointsLength = 0
+        }
+
+    }
     addListeners() {
         this.viewer.cesiumWidget.container.classList.toggle("cursor-crosshair")
         //add left-click listener
-        this.eventHandler.setInputAction((event: any) => {
-            if (this.mode === 'draw') {
-                this.isDrawingActive = true
-                var newPosition = this.viewer.camera.pickEllipsoid(event.position);
-                if (!this.currentDrawingShape) {
-                    var dynamicPositions = new CallbackProperty((() => {
-                        return { positions: this.shapePoints };
-                    }), false);
-                    this.currentDrawingShape = this.viewer.entities.add({
-                        polygon: {
-                            hierarchy: dynamicPositions,
-                            height: 0,
-                            material: Color.RED.withAlpha(0.5),
-                            outline: true,
-                            outlineWidth:4,
-                            outlineColor: Color.BLACK,
-                        },
-                    });
-                }
-                this.shapePoints.push(newPosition as Cartesian3)
-                this.shapePointsLength++
-            } else if (this.mode === "select") {
-                let picked = this.viewer.scene.pick(event.position)
-                console.log("p->", picked)
-                this.onSelectObject(picked?.id)
-            }
-
-        }, ScreenSpaceEventType.LEFT_CLICK)
-
-        
-        this.eventHandler.setInputAction((event: any) => {
-            let entity: Entity = this.viewer.scene.pick(event.position)
-            if (entity) {
-                this.isDraging = true
-                this.startDragingPostion = event.position
-                this.viewer.scene.screenSpaceCameraController.enableRotate = false
+        this.eventHandler.setInputAction(this.onLeftClick.bind(this), ScreenSpaceEventType.LEFT_CLICK)
 
 
-            }
-        }, ScreenSpaceEventType.LEFT_DOWN)
-        this.eventHandler.setInputAction((event: any) => {
-            this.isDraging = false
-            this.viewer.scene.screenSpaceCameraController.enableRotate = true
-        }, ScreenSpaceEventType.LEFT_UP)
+        this.eventHandler.setInputAction(this.onLeftDown.bind(this), ScreenSpaceEventType.LEFT_DOWN)
+        this.eventHandler.setInputAction(this.onLeftUp.bind(this), ScreenSpaceEventType.LEFT_UP)
         //add mouse-move listener 
-        this.eventHandler.setInputAction((event: any) => {
-            if (this.isDrawingActive) {
-                var newPosition = this.viewer.camera.pickEllipsoid(event.endPosition);
-                if (this.shapePointsLength < this.shapePoints.length)
-                    this.shapePoints.pop()
-
-                this.shapePoints.push(newPosition as Cartesian3)
-            }
-            if (this.isDraging && this.startDragingPostion && this.selectedEntitiy && this.selectedLatLngInitPostions.length) {
-                //@ts-ignore
-                let rotationAngle = this.getRotationAngel(this.selectedCenter,
-                    this.viewer.camera.pickEllipsoid(this.startDragingPostion),
-                    this.viewer.camera.pickEllipsoid(event.endPosition)
-                )
-                let rotatedPolygon = transformRotate(polygon([[...this.selectedLatLngInitPostions, this.selectedLatLngInitPostions[0]]]), rotationAngle, { pivot: this.selectedCenter })
-                let Cart3Coordinate = rotatedPolygon.geometry.coordinates[0].map(ele => {
-                    return Cartesian3.fromDegrees(ele[0], ele[1])
-                })
-                Cart3Coordinate.pop()
-
-                if (this.selectedEntitiy)
-                    this.selectedPostions.positions = Cart3Coordinate
-
-            }
-            // 
-
-        }, ScreenSpaceEventType.MOUSE_MOVE)
+        this.eventHandler.setInputAction(this.onMouseMove.bind(this), ScreenSpaceEventType.MOUSE_MOVE)
 
         // add double-click listener
-        this.eventHandler.setInputAction((event: any) => {
-            if (this.isDrawingActive) {
-                this.isDrawingActive = false
-                let points = { positions: [...this.shapePoints] }
-                let entity = this.viewer.entities.add({
-                    polygon: {
-                        hierarchy: new CallbackProperty(() => {
-                            return { positions: points.positions }
-                        }, false)
-                        ,
-                        height: 0,
-                        material: Color.WHITE.withAlpha(0),
-                        outlineWidth:9,
-                        outline: true,
-                        outlineColor: Color.BLACK,
-                    },
-
-                });
-                //@ts-ignore
-                entity.points = points
-                if (this.currentDrawingShape) {
-                    this.viewer.entities.remove(this.currentDrawingShape)
-                }
-
-                this.currentDrawingShape = null
-                this.shapePoints = []
-                this.shapePointsLength = 0
-            }
-  
-        }, ScreenSpaceEventType.LEFT_DOUBLE_CLICK)
+        this.eventHandler.setInputAction(this.onDoubleClick.bind(this), ScreenSpaceEventType.LEFT_DOUBLE_CLICK)
 
     }
     getcoordinatesForBoundingBox(points: Array<Cartesian3>) {
@@ -197,7 +198,7 @@ export class DrawingTool {
         })
         return result
     }
-    onSelectObject(selectedEntity: Entity) {
+    private onSelectObject(selectedEntity: Entity) {
         this.removeSelectionBoundry()
         this.selectedEntitiy = null
         if (this.mode === 'select' && selectedEntity && selectedEntity.polygon) {
@@ -284,7 +285,7 @@ export class DrawingTool {
     }
     changeColor() {
         if (this.selectedEntitiy?.polygon?.outlineColor)
-        //@ts-ignore
+            //@ts-ignore
             this.selectedEntitiy.polygon.outlineColor = Color.GREEN
     }
 
